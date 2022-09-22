93240e909291388e0e373c1a3ed15f150185c4c1
周翔 <keyneko@163.com>
2022-09-22 18:12:45 +0800

    .

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,33 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, mount } from '@vue/test-utils'
+import HelloWorld from '@/components/HelloWorld'
+import Counter from '@/components/Counter'
 
 describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
+  it('正确渲染msg属性', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+
+    expect(wrapper.text()).toContain(msg)
+  })
+})
+
+describe('Counter.vue', () => {
+  const wrapper = mount(Counter)
+
+  it('页面正确渲染', () => {
+    expect(wrapper.html()).toContain('<span class="count">0</span>')
+  })
+
+  it('页面有一个按钮', () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+
+  it('点击按钮应增加计数', () => {
+    expect(wrapper.vm.count).toBe(0)
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.count).toBe(1)
   })
 })
