122a45e71c71afbaf966277990c2d5fe97c6367e
周翔 <keyneko@163.com>
2022-09-23 16:06:39 +0800

    .

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,29 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest'
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.vue$": "@vue/vue2-jest"
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  snapshotSerializers: ['jest-serializer-vue'],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)',
+    '**/__tests__/**/*.spec.(js|jsx|ts|tsx)',
+  ],
+  collectCoverageFrom: [
+    'src/utils/**/*.{js,vue}', 
+    '!src/utils/auth.js', 
+    '!src/utils/request.js', 
+    'src/components/**/*.{js,vue}'
+  ],
+  coverageDirectory: '<rootDir>/tests/unit/coverage',
+  'collectCoverage': true,
+  'coverageReporters': [
+    'html',
+    'text-summary',
+  ],
+  testURL: 'http://localhost/'
 }
--- src/components/__tests__/Counter.spec.js
+++ src/components/__tests__/Counter.spec.js
@@ -9,7 +9,7 @@ describe('Counter.vue', () => {
   })
 
   it('页面有一个按钮', () => {
-    const button = wrapper.find('.counter button') 
+    const button = wrapper.find('.counter button')
     expect(button.exists()).toBe(true)
   })
 
@@ -20,14 +20,14 @@ describe('Counter.vue', () => {
     expect(wrapper.vm.count).toBe(1)
   })
 
-  it('点击按钮应增加计数2', async () => {
+  it('点击按钮应增加计数2', async() => {
     expect(wrapper.text()).toContain('1')
     const button = wrapper.find('.counter button')
     await button.trigger('click')
     expect(wrapper.text()).toContain('2')
   })
 
-  it('修改状态', async () => {
+  it('修改状态', async() => {
     expect(wrapper.vm.count).toBe(2)
     await wrapper.setData({ count: 10 })
     expect(wrapper.vm.count).not.toBe(2)
